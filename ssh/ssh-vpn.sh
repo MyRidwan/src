1e17c8cbe41ae9da8a5e82934826921b12e6af9b
X£©GANSZ™ <92353970+MyRidwan@users.noreply.github.com>
2022-11-08 20:03:09 +0700

    Update ssh-vpn.sh

--- ssh/ssh-vpn.sh
+++ ssh/ssh-vpn.sh
@@ -304,6 +304,7 @@ wget -O trial-menu "https://raw.githubusercontent.com/myridwan/vip/ipuk/menu_all
 wget -O info-menu "https://raw.githubusercontent.com/myridwan/vip/ipuk/menu_all/info-menu.sh"
 wget -O ceklim "https://raw.githubusercontent.com/myridwan/src/ipuk/ssh/ceklim.sh"
 wget -O cekusage "https://raw.githubusercontent.com/myridwan/src/ipuk/xray/cekusage.sh"
+wget -O cekusage "https://raw.githubusercontent.com/myridwan/src/ipuk/cekxray.sh"
 chmod +x usernew
 chmod +x menu
 chmod +x hapus
@@ -329,6 +330,7 @@ chmod +x trial-menu
 chmod +x info-menu
 chmod +x ceklim
 chmod +x cekusage
+chmod +x cekxray
 cd
 
 
